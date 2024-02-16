edc8f99a0364018f30bc45f15e08b179d5f1de91
mp30028 <mp30028@gmail.com>
2024-02-16 07:46:00 +0000

    changed to use named props

--- modules/example-ui/frontend/src/lib/raw-display/RawDisplay.js
+++ modules/example-ui/frontend/src/lib/raw-display/RawDisplay.js
@@ -1,17 +1,17 @@
 import React, {useState, useEffect} from 'react';
 
-const RawDisplay = (props) => {
+const RawDisplay = ({dataService}) => {
 	
 	const [data, setData] = useState(null);
 	
 	useEffect(() => {
 		const empty = {id:"temp-id-000", message :"No data available"};
-		if (props.dataService){
-			props.dataService.fetchAll().then((data) => setData(data));
+		if (dataService){
+			dataService.fetchAll().then((data) => setData(data));
 		}else{
 			setData(empty);
 		}		
-	}, [setData,props.dataService]);	
+	}, [setData, dataService]);	
 	
 	return (
 		<pre>
