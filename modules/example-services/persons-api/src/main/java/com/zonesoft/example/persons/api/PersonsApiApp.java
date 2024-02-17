efede3a2853306a5ef3867d39684b3c26a1b1e5d
mp30028 <mp30028@gmail.com>
2024-02-17 05:45:53 +0000

    added module for persons-api

---
+++ modules/example-services/persons-api/src/main/java/com/zonesoft/example/persons/api/PersonsApiApp.java
@@ -0,0 +1,13 @@
+package com.zonesoft.example.persons.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonsApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonsApiApp.class, args);
+	}
+
+}
