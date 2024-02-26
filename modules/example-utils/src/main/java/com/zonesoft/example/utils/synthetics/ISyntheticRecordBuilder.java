1674a3f816450302d3ea33eb17b4706b9d1f5018
mp30028 <mp30028@gmail.com>
2024-02-26 04:41:36 +0000

    extended interface with clone

--- modules/example-utils/src/main/java/com/zonesoft/example/utils/synthetics/ISyntheticRecordBuilder.java
+++ modules/example-utils/src/main/java/com/zonesoft/example/utils/synthetics/ISyntheticRecordBuilder.java
@@ -1,7 +1,10 @@
 package com.zonesoft.example.utils.synthetics;
 
-public interface ISyntheticRecordBuilder <T, B> {
+import java.io.Serializable;
+
+public interface ISyntheticRecordBuilder  <T, B> extends Serializable, Cloneable {
 	public T build();
 	public B withDefaults();
 	public B withDefaults(boolean withId);
+	public B clone(T source);
 }
