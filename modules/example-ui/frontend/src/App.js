dfb074308e52b7906b870057e471b39279a3efe9
mp30028 <mp30028@gmail.com>
2024-03-02 07:53:04 +0000

    added cdn import of react-bootstrap styles

--- modules/example-ui/frontend/src/App.js
+++ modules/example-ui/frontend/src/App.js
@@ -4,6 +4,12 @@ import Home from 'ui/home';
 
 const App = () => {	return (
 		<>
+			<link
+			  rel="stylesheet"
+			  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+			  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+			  crossorigin="anonymous"
+			/>		
 			<Routes>
 				<Route path="/ui/main-app" element={<Home />} /> 
 				<Route path="*" element={<p>There's nothing here: 404!</p>} />			
