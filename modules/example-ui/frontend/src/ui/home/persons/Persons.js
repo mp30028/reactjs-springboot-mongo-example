980b517f5746d1110fc114bb11630939c3556254
mp30028 <mp30028@gmail.com>
2024-03-02 07:54:39 +0000

    updated Persons component to use ListDisplay component

--- modules/example-ui/frontend/src/ui/home/persons/Persons.js
+++ modules/example-ui/frontend/src/ui/home/persons/Persons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DataService from 'lib/data-service';
 import ApiClientConfigs from 'lib/api-client-configs';
-import RawDisplay from 'lib/raw-display';
+import ListDisplay from 'lib/list-display';
 
 const Persons = () => {
 	const CONFIG_KEY = "Persons";
@@ -9,7 +9,7 @@ const Persons = () => {
 	
 	return (
 		<>
-			<RawDisplay  dataService={dataService} />
+			<ListDisplay  dataService={dataService} />
 		</>	
 	)
 
