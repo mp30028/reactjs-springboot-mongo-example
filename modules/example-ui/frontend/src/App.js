492be3a648b9b4c9935131108397caef97f55a8c
mp30028 <mp30028@gmail.com>
2024-03-07 04:37:18 +0000

    added bootstrap and other dependencies

--- modules/example-ui/frontend/src/App.js
+++ modules/example-ui/frontend/src/App.js
@@ -4,12 +4,6 @@ import Home from 'ui/home';
 
 const App = () => {	return (
 		<>
-			<link
-			  rel="stylesheet"
-			  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-			  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-			  crossOrigin="anonymous"
-			/>		
 			<Routes>
 				<Route path="/ui/main-app" element={<Home />} /> 
 				<Route path="*" element={<p>There's nothing here: 404!</p>} />			
