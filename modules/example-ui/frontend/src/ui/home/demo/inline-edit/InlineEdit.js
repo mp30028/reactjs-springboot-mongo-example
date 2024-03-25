a3a276ffc3b2e76f7045678a17004d2cccb991fd
mp30028 <mp30028@gmail.com>
2024-03-25 22:57:14 +0000

    added reset button

--- modules/example-ui/frontend/src/ui/home/demo/inline-edit/InlineEdit.js
+++ modules/example-ui/frontend/src/ui/home/demo/inline-edit/InlineEdit.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logger, {level} from 'lib/logger';
+import TextEdit from 'lib/text-edit';
 
 const InlineEdit = () => {
-	const LOGGER = Logger('InlineEdit', level.DEBUG);
+	const LOGGER_NAME = 'InlineEdit';
+	const LOGGER = Logger(LOGGER_NAME, level.DEBUG);
+	const DEFAULT_VALUE = "This is a default value";
+	const [currentValue, setCurrentValue] = useState(DEFAULT_VALUE);
+	
+	const onUpdateHandler = (update) =>{
+		LOGGER.debug(LOGGER.name, "onUpdateHandler: before update of current value",  {update: update}, {currentValue: currentValue});
+		setCurrentValue(update.updatedValue);
+	}
+	
+	useEffect(()=>{
+		const LOGGER = Logger(LOGGER_NAME);
+		LOGGER.debug(LOGGER.name, "currentValue-Hook",  {currentValue: currentValue});
+	},[currentValue])
+	
+	const doReset = () =>{
+		setCurrentValue(DEFAULT_VALUE);
+	}
 	
 	return (
 		<>
-			Hello from InLine Edit Demo
+			<TextEdit fieldName='Input'
+				displayLabel='Input text'  
+				currentValue={currentValue} 
+				onSaveHandler={onUpdateHandler} 
+			/>
+			
+			<input type='button' id='resetButton' name= 'resetButton' onClick={doReset} value="reset" />
 		</>	
 	)
 
