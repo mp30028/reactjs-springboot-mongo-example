5b2278ab631f46fa6615ad66dec4aabd14d3ae59
mp30028 <mp30028@gmail.com>
2024-03-27 01:53:32 +0000

    added place holder for inline-combo-edit demo

--- modules/example-ui/frontend/src/ui/home/demo/Demo.js
+++ modules/example-ui/frontend/src/ui/home/demo/Demo.js
@@ -2,26 +2,32 @@ import React from 'react';
 
 import SimpleGreeting from 'ui/home/demo/simple-greeting';
 import Greetings from 'ui/home/demo/greetings';
-import InlineEdit from 'ui/home/demo/inline-edit';
+import InlineTextEdit from 'ui/home/demo/inline-text-edit';
+import InlineComboEdit from 'ui/home/demo/inline-combo-edit';
 
 const Demo = () => {
 	
 	return (
 		<>
 			<span>
-				<h2>Demo-1. Inline Edit</h2>
-				<InlineEdit />				
+				<h2>Demo-1. Inline Text Edit</h2>
+				<InlineTextEdit />				
 			</span>
 			<hr/>
 			<span>
-				<h2>Demo-2</h2>
+				<h2>Demo-2. Inline Combo Edit</h2>
+				<InlineComboEdit />				
+			</span>
+			<hr/>			
+			<span>
+				<h2>Demo-3</h2>
 				Invoke the Simple-Greeting-API
 				<h3>Service Response:</h3> 
 				<SimpleGreeting/>
 			</span>
 			<hr/>
 			<span>
-				<h2>Demo-3</h2>
+				<h2>Demo-4</h2>
 				Invoke the Greetings-API
 				<h3>Service Response:</h3> 
 				<Greetings/>
